71bd8ed6ab87b59180145f01db15266ae01dafa0
徵 <liao_brant@163.com>
2022-02-15 15:30:23 +0800

    dist

---
+++ dist/assets/index.62543573.js
@@ -0,0 +1,1 @@
+import{n as _,o as t,c as o}from"./vendor.88f433db.js";import{_ as n}from"./index.57c54ce9.js";const a={name:"WhyPage"};function r(c,s,p,u,d,i){const e=_;return t(),o(e,{description:"\u4E3A\u4F55\u6765\u5230\u6B64\u5730?"})}var l=n(a,[["render",r],["__scopeId","data-v-5075054f"]]);export{l as default};
