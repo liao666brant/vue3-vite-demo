6b7b5129ae7e361438b64f94d15dee7cdc635b73
徵 <liao_brant@163.com>
2022-04-11 09:26:18 +0800

    chore: np

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,11 @@
 /**
- * upd：更新某功能（不是 feat, 不是 fix）
  * feat：新功能（feature）
  * fix：修补bug
  * docs：文档（documentation）
- * style： 格式（不影响代码运行的变动）
+ * style：不影响程序逻辑的代码修改(修改空白字符，补全缺失的分号等), 注意不是 css 修改
  * refactor：重构（即不是新增功能，也不是修改bug的代码变动）
  * test：增加测试
- * chore：构建过程或辅助工具的变动
+ * chore：构建过程或辅助工具的变动,不属于以上类型的其他类型(日常事务)
  */
 
 module.exports = {
